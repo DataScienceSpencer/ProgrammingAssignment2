40b8f695560e23e01dbe2c47f4c25af51d576901
Spencer <spencer.hensley@gmail.com>
2014-06-17 17:01:19 -0700

    Added code

--- Assignment2.R
+++ Assignment2.R
@@ -2,20 +2,21 @@
 ## and then cache its inverse to avoid calculating that inverse multiple
 ## times. 
 
+
 ## makeCacheMademotrix will create an empty framework into which you can
 ## create the matrix you want to calculate the inverse of. 
 
 makeCacheMatrix <- function(x = matrix()) { 
-        m <- NULL
-        set <- function(y){  
-                # x$set will allow you to set the values of your matrix
-                x <<- y
-                m <<- NULL
-        }
-        get <- function () x
-        setinverse <- function(inverse) m <<- inverse
-        getinverse <- function() m
-        list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
+  m <- NULL
+  set <- function(y){  
+    # x$set will allow you to set the values of your matrix
+    x <<- y
+    m <<- NULL
+  }
+  get <- function () x
+  setinverse <- function(inverse) m <<- inverse
+  getinverse <- function() m
+  list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
 
 ## cacheSolve will check to see if your matrix is empty, and if so
@@ -24,15 +25,15 @@ makeCacheMatrix <- function(x = matrix()) {
 ## is already cached there), it will return the cached data. 
 
 cacheSolve <- function (x, ...) {
-        m <- x$getinverse()
-        if(!is.null(m)) {
-                message("getting cached data")
-                return(m)
-        }
-        data <- x$get()
-        m <- solve(data, ...)
-        x$setinverse(m)
-        m
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
 }
 
 ## The following tests the above functions.
@@ -43,3 +44,4 @@ test$get()
 
 cacheSolve(test)
 cacheSolve(test)
+
